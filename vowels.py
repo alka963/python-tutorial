8b308e1899024dca0429aa5ccab5caf6bc05c5aa
unknown <admin@CHETUIWK980.chetu.com>
2021-10-07 16:58:54 +0530

    volel

---
+++ vowels.py
@@ -0,0 +1,16 @@
+'''name = 'Amit Kumar'
+lower = 0
+upper = 0
+for i in name:
+    if i.isupper():
+        upper = upper + 1
+    elif i.islower():
+        lower = lower + 1
+print('lower',lower)
+print('upper',upper)
+'''
+name='Chetu'
+vowels=0
+for i in name
+if(i =='a'or i =='e' or i == 'i' or i == 'o' or i == 'u')
+vowel
